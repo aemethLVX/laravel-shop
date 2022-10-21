0272d0c52a338c7a5de94db303911a7afbba33ca
Aemeth <aemeth.lvx@gmail.com>
2022-10-21 19:01:49 +0300

    Add Telegram logger

--- app/Logging/Telegram/TelegramLoggerFactory.php
+++ app/Logging/Telegram/TelegramLoggerFactory.php
@@ -8,7 +8,7 @@ class TelegramLoggerFactory
 {
     public function __invoke(array $config): Logger
     {
-        $logger = new Logger('telegram');
+        $logger = new Logger('channels.telegram');
         $logger->pushHandler(new TelegramLoggerHandler($config));
 
         return $logger;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,43 +4,27 @@ namespace App\Providers;
 
 use Carbon\CarbonInterval;
 use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         Model::preventLazyLoading(!app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
 
         if (!app()->isProduction()) {
-            DB::whenQueryingForLongerThan(500, function (Connection $connection) {
-                logger()->channel('telegram')->debug(
-                    'whenQueryingForLongerThan: ' . $connection->query()->toSql()
-                );
+            DB::listen(function ($query) {
+                if ($query->time > 1000) {
+                    logger()->channel('telegram')->debug(
+                        "Query longer than 1s: {$query->sql} {$query->bindings}"
+                    );
+                }
             });
 
-            $kernel = app(Kernel::class);
-
-            $kernel->whenRequestLifecycleIsLongerThan(
+            app(Kernel::class)->whenRequestLifecycleIsLongerThan(
                 CarbonInterval::seconds(4),
                 function () {
                     logger()->channel('telegram')->debug(
